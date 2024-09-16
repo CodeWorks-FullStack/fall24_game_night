ff7100cd71526b15b59ee1de89f38c46c0df1fe3
Jeremy Fowler <jembyfowler@gmail.com>
2024-09-16 12:09:35 -0600

    creating player

--- src/components/globals/PlayerForm.vue
+++ src/components/globals/PlayerForm.vue
@@ -1,15 +1,26 @@
 <script setup>
+import { playersService } from '@/services/PlayersService.js';
 import { ref } from 'vue';
 
 const editablePlayerData = ref({
   name: '',
   imgUrl: ''
 })
+
+function createPlayer() {
+  console.log('creating player! Player data:', editablePlayerData.value);
+  playersService.createPlayer(editablePlayerData.value)
+  editablePlayerData.value = {
+    name: '',
+    imgUrl: ''
+  }
+}
 </script>
 
 
 <template>
-  <form>
+  <!-- NOTE .prevent will do event.preventDefault() -->
+  <form @submit.prevent="createPlayer()">
     <div class="form-floating mb-3">
       <input v-model="editablePlayerData.name" type="text" class="form-control" id="name" placeholder="Player Name..."
         required maxlength="20">
@@ -20,7 +31,7 @@ const editablePlayerData = ref({
         placeholder="Player Image..." required maxlength="1000">
       <label for="imgUrl">Player Image</label>
     </div>
-    <button class="btn btn-success w-100">Add Player</button>
+    <button class="btn btn-success w-100" type="submit">Add Player</button>
   </form>
 </template>
 
--- src/services/PlayersService.js
+++ src/services/PlayersService.js
@@ -1,4 +1,12 @@
+import { AppState } from "@/AppState.js"
+import { Player } from "@/models/Player.js"
+
 class PlayersService {
+  createPlayer(playerData) {
+    const players = AppState.players
+    const player = new Player(playerData)
+    players.push(player)
+  }
   decreaseScore(player) {
     if (player.score < 1) {
       return
