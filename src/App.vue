15ec90fd8ad3d72d73aa2bd427ef718a0f674b34
Jeremy Fowler <jembyfowler@gmail.com>
2024-09-16 10:55:26 -0600

    v-forring over players

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup>
 import { ref } from 'vue'
+import { AppState } from './AppState.js'
 // NOTE anything declared in here is referred to as component state
 
 const score = ref(0)
+
+const players = AppState.players
 
 function decreaseScore() {
   if (score.value > 0) {
@@ -25,32 +28,15 @@ function increaseScore() {
         </div>
       </section>
       <section class="row">
-        <div class="col-lg-6">
+        <div v-for="player in players" :key="player.name" class="col-lg-6 mb-3">
           <div class="d-flex justify-content-between align-items-center">
             <div class="d-flex gap-3 align-items-center">
               <img
                 src="https://images.unsplash.com/photo-1707765643599-8c60886bf52b?q=80&w=2265&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="Dude on the sidewalk" class="img-fluid player-img">
-              <h2>Dude</h2>
+              <h2>{{ player.name }}</h2>
             </div>
             <div class="d-flex align-items-center gap-3">
-              <!-- NOTE @click is vue's version of onclick -->
-              <button @click="decreaseScore()" class="btn btn-dark fs-3">-</button>
-              <span class="fs-2">{{ score }}</span>
-              <button @click="increaseScore()" class="btn btn-dark fs-3">+</button>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-6">
-          <div class="d-flex justify-content-between align-items-center">
-            <div class="d-flex gap-3 align-items-center">
-              <img
-                src="https://images.unsplash.com/photo-1707765643599-8c60886bf52b?q=80&w=2265&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="Dude on the sidewalk" class="img-fluid player-img">
-              <h2>Dude</h2>
-            </div>
-            <div class="d-flex align-items-center gap-3">
-              <!-- NOTE @click is vue's version of onclick -->
               <button @click="decreaseScore()" class="btn btn-dark fs-3">-</button>
               <span class="fs-2">{{ score }}</span>
               <button @click="increaseScore()" class="btn btn-dark fs-3">+</button>
--- src/AppState.js
+++ src/AppState.js
@@ -9,7 +9,8 @@ export const AppState = reactive({
   account: null,
   players: [
     new Player({ name: 'Dude', imgUrl: 'https://images.unsplash.com/photo-1707765643599-8c60886bf52b?q=80&w=2265&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' }),
-    new Player({ name: 'Mick', imgUrl: 'https://images.unsplash.com/photo-1552656504-5fd2ed591222?q=80&w=2254&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' })
+    new Player({ name: 'Mick', imgUrl: 'https://images.unsplash.com/photo-1552656504-5fd2ed591222?q=80&w=2254&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' }),
+    new Player({ name: 'Jake', imgUrl: 'https://images.unsplash.com/photo-1472785316312-26b92b2fccdd?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' })
   ]
 })
 
